b973cdad212cb915c0a7a85f308fa6d85dc9a7a1
Tom Berry <tberry860@gmail.com>
2017-11-23 17:40:04 -0800

    fix for rn upgrade

--- android/src/main/java/com/airbnb/android/react/maps/MapsPackage.java
+++ android/src/main/java/com/airbnb/android/react/maps/MapsPackage.java
@@ -24,7 +24,6 @@ public class MapsPackage implements ReactPackage {
         return Arrays.<NativeModule>asList(new AirMapModule(reactContext));
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
